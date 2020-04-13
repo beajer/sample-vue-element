c3b99591a670919fdda85f5c56e096f6b4eefa52
beajer <919060679@qq.com>
2020-04-14 01:12:25 +0800

    init

---
+++ src/layout/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="classObj" class="app-wrapper">
+    <sidebar :isCollapse="!isSiderbarOpen" class="sidebar-container" />
+    <div class="main-container">
+      <div>
+        <navbar :isActive="isSiderbarOpen" @toggleSideBar="toggleSideBar" />
+      </div>
+      <app-main />
+    </div>
+  </div>
+</template>
+
+<script>
+import { Navbar, Sidebar, AppMain } from "./components";
+
+export default {
+  name: "Layout",
+  components: {
+    Navbar,
+    Sidebar,
+    AppMain
+  },
+  data() {
+    return {
+      isSiderbarOpen: true
+    };
+  },
+  methods: {
+    toggleSideBar() {
+      this.isSiderbarOpen = !this.isSiderbarOpen;
+    }
+  },
+  computed: {
+    classObj() {
+      return {
+        hideSidebar: !this.isSiderbarOpen,
+        openSidebar: this.isSiderbarOpen,
+        withoutAnimation: false
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/mixin.scss";
+@import "~@/styles/variables.scss";
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/views/404.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>该页面找不到了</div>
+</template>
